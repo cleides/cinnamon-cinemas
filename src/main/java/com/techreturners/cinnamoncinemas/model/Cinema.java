0bf5bf169c479f0f302ce8ae87574ac4e3c370ab
CleideS <40967363+cleides@users.noreply.github.com>
2022-02-14 22:54:17 +0000

    Test Driven Development - checkSeatsAvailable - RED

--- src/main/java/com/techreturners/cinnamoncinemas/model/Cinema.java
+++ src/main/java/com/techreturners/cinnamoncinemas/model/Cinema.java
@@ -1,4 +1,20 @@
 package com.techreturners.cinnamoncinemas.model;
 
 public class Cinema {
+
+    private boolean[][] seats = new boolean[3][5];
+
+    public String checkAndAllocateSeats(int desiredSeatsCount) {
+        return "";
+    }
+    // Seats must be available before allocation
+    public boolean checkSeatsAvailable(int desiredSeatsCount) {
+        return false;
+    }
+    //encapsulated because this method can only be run safely if
+    // seatsAvailable has passed
+    private String allocateSeats(int desiredSeatsCount) {
+        return "";
+    }
+
 }
---
+++ src/test/java/com/techreturners/cinnamoncinemas/model/CinemaTest.java
@@ -0,0 +1,22 @@
+package com.techreturners.cinnamoncinemas.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CinemaTest {
+
+    @Test
+    void testCheckAndAllocateSeats() {
+    }
+
+    @Test
+    void testCheckSeatsAvailable() {
+        //Arrange
+        Cinema sut = new Cinema();
+        //Act
+        boolean areSeatsAvailable = sut.checkSeatsAvailable(3);
+        //Assert
+        assertTrue(areSeatsAvailable);
+    }
+}
